9ddbb7c2feabfb23ab5140457447aa2e3fba6e2e
unknown <weifang060@gmail.com>
2026-03-29 16:34:09 +1300

    Modify Start file

--- start.ps1
+++ start.ps1
@@ -1,39 +1,83 @@
 param(
-  $env = "dev"
+  [string]$env = "dev"
+  [string]$project = "all" #frontend, backend, all
+
 )
 
 Set-Location $PSScriptBoot
 
+function Write-Log {
+  param([string]$Message)
+
+  $time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+  $LogFile = "$PsscriptBoot\project.log"
+
+  $output = "$time - $Message"
+
+  Add-Content -Path $LogFile -Value $output
+  Write-Output $output
+}
+
 function Update-Code {
   if (Test-Path ".git") {
-    Write-Output "Pulling latest code..."
-    git pull
+    try {
+        Write-Log "Pulling latest code..."
+        git pull
+    }
+    catch {
+        Write-Log "Error occurred while pulling code."
+    }
   }
 }
 
 function Install-Dependencies {
+  try {
+    if (Test-Path "package.json") {
+        Write-Output "Installing dependencies..."
+        if (!(Test-Path "node modules")) {
+        npm install
+    }
+  }
+  catch {
+    Write-Log "Dependency installation failed."
+  }
+}
+
+function Start-Frontend {
   if (Test-Path "package.json") {
-    Write-Output "Installing dependencies..."
-    if (!(Test-Path "node modules")) {
-      npm install
+    try {
+        Write-Output "Starting Frontend..."
+        npm run dev
+    }
+    catch {
+        Write-Log "Frontend failed: $_"
     }
   }
 }
 
-function Start-App {
-  if (Test-Path "package.json") {
-    if ($env -eq "dev") {
-      Write-Output "Starting dev server..."
-      npm run dev
-      Start-Process "http://localhost:3000"
+function Start-Backend {
+  if (Get-ChildItem *.csproj -ErrorAction SilentlyContinue) {
+    try {
+        Write-Output "Starting Backend..."
+        donet run
     }
-    elseif ($env -eq "prod") {
-      Write-Output "Starting production..."
-      npm sratr
+    catch {
+        Write-Log "Backend failed: $_"
     }
   }
 }
+
+Write-Log "Starting project in $env mode..."
 
 Update-Code
 Install-Dependencies
-Start-App
+
+if ($project -eq "frontend" -or $project -eq "all") {
+  Start-Frontend
+}
+
+if ($project -eq "backend" -or $project -eq "all") {
+  Start-Backend
+}
+
+Write-Log "Script finished"
